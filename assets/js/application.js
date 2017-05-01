296c039b02f1a485bf352997f9bc2a0e05a315cd
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-05-01 15:28:56 -0500

    smooth scrolling initiated

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	emailToMe();
+	smoothScroll();
 });
 
 var emailToMe = function(){
@@ -10,3 +11,19 @@ var emailToMe = function(){
 		document.location = 'mailto:'+email+''
 	});
 };
+
+var smoothScroll = function(){
+	$('a[href^="#"]').on('click', function(e){
+		e.preventDefault();
+
+		var target = this.hash
+		var $target = $(target);
+
+		$('body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 900, 'swing', function() {
+			window.location.hash = target;
+		});
+	});
+	
+}
